9b6c9d15bc2f75ff866804dbeebd7c16d425bbf9
shenlan <xz@onwalk.net>
2021-01-28 18:37:02 +0800

    update Kafka/kafka-install.sh

--- deploy-for-k8s/Kafka/kafka-install.sh
+++ deploy-for-k8s/Kafka/kafka-install.sh
@@ -1,3 +1,7 @@
+wget https://get.helm.sh/helm-v3.5.0-linux-amd64.tar.gz
+tar -zxvf helm-v3.5.0-linux-amd64.tar.gz
+cp linux-amd64/helm /usr/bin/helm
+
 helm repo add stable http://mirror.azure.cn/kubernetes/charts
 helm repo add incubator http://mirror.azure.cn/kubernetes/charts-incubator
 helm install --name kafka --namespace kafka -f kafka-values.yaml incubator/kafka
